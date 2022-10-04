e17cbe0a0a9db6386c0dd3ff8cec05cdb34b3d90
Antonio <antonio.ti.celso@gmail.com>
2022-10-04 12:28:17 -0300

    ProdutoImpl - criação de consultas

--- lojaDemo/src/main/java/com/github/antonioticelso/lojademo/LojaDemoApplication.java
+++ lojaDemo/src/main/java/com/github/antonioticelso/lojademo/LojaDemoApplication.java
@@ -8,12 +8,32 @@ import com.github.antonioticelso.lojademo.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
 
 //@SpringBootApplication
 public class LojaDemoApplication {
 
 	public static void main(String[] args) {
 //		SpringApplication.run(LojaDemoApplication.class, args);
+		cadastrarProduto();
+		EntityManager manager = JPAUtil.getEntityManager();
+		ProdutoImpl produtoImpl = new ProdutoImpl(manager);
+
+		Produto p = produtoImpl.buscarPorId(1l);
+		System.out.println(p.getName() + " - " + p.getPreco()+ " - " + p.getCategoria());
+
+		List<Produto> list = produtoImpl.listAll();
+		list.forEach(pr -> System.out.println(pr.getName()));
+
+		List<Produto> todos = produtoImpl.buscarPorNome("Xiaomi Redmi Pro 8");
+		list.forEach(pr -> System.out.println(pr.getName()));
+
+		List<Produto> todaCategoria = produtoImpl.buscarPorNomeCategoria("Xiaomi Redmi Pro 8");
+		list.forEach(pr -> System.out.println(pr.getName()));
+
+	}
+
+	private static void cadastrarProduto() {
 		EntityManager manager = JPAUtil.getEntityManager();
 
 		ProdutoImpl produtoImpl = new ProdutoImpl(manager);
@@ -35,7 +55,6 @@ public class LojaDemoApplication {
 
 		manager.getTransaction().commit();
 		manager.close();
-
 	}
 
 }
--- lojaDemo/src/main/java/com/github/antonioticelso/lojademo/repository/ProdutoImpl.java
+++ lojaDemo/src/main/java/com/github/antonioticelso/lojademo/repository/ProdutoImpl.java
@@ -5,6 +5,7 @@ import com.github.antonioticelso.lojademo.modelo.Produto;
 import lombok.AllArgsConstructor;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @AllArgsConstructor
 public class ProdutoImpl {
@@ -22,6 +23,31 @@ public class ProdutoImpl {
     public void remover(Produto produto) {
         atualizar(produto);
         this.manager.remove(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+       return manager.find(Produto.class, id);
+    }
+
+    public List<Produto> listAll() {
+        String jpql = "SELECT p FROM Produto p";
+        return manager.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome) {
+//        String jpql = "SELECT p FROM Produto p WHERE p.name = ?1";
+        String jpql = "SELECT p FROM Produto p WHERE p.name = :nome";
+        return manager.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeCategoria(String nome) {
+//        String jpql = "SELECT p FROM Produto p WHERE p.name = ?1";
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return manager.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
     }
 
 }
