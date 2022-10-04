bcec9fe830ffa514895596edd33d418f6afdbc3e
Antonio <antonio.ti.celso@gmail.com>
2022-10-04 19:03:37 -0300

    Pedido - criação de pedidos

---
+++ lojaDemo/src/main/java/com/github/antonioticelso/lojademo/modelo/Pedido.java
@@ -0,0 +1,26 @@
+package com.github.antonioticelso.lojademo.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pedidos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate data = LocalDate.now();
+
+    @ManyToOne
+    private Cliente cliente;
+    private double valorTotal;
+
+}
