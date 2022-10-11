e4466172cb2f26782977877a191bf073573b5ba6
Antonio <antonio.ti.celso@gmail.com>
2022-10-11 14:53:17 -0300

    First commit

---
+++ springData/src/test/java/com/github/antonioticelso/springData/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.springData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
