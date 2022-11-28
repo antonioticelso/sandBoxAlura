ba1088d8712decbfd47accca8108534b7a8dd615
Antonio <antonio.ti.celso@gmail.com>
2022-11-28 16:44:16 -0300

    Mudi - First commit

---
+++ mudi/src/test/java/com/github/antonioticelso/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
